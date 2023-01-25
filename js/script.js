eb98d7b2a46b2932074b38e1eb064b869f250f6d
KoLario <felix.wong@gritacademy.se>
2023-01-25 18:00:22 +0100

    added a small error check

--- js/script.js
+++ js/script.js
@@ -121,32 +121,43 @@ function getTimestamp() {
 }
 
 //Search functions, display in a container and show the total hits of matching word
-const searchInput = document.querySelector('#search-input');
-const searchBtn = document.querySelector('#search-btn');
-
 onValue(ref(db, '/'), (snapshot) => {
+    const searchInput = document.querySelector('#search-input');
+    const searchBtn = document.querySelector('#search-btn');
+    const searchErrorText = document.querySelector('.search-error-text');
+    const searchResultsContainer = document.querySelector('#search-results-container');
+    const searchResultCount = document.querySelector('#search-result-count');
+    
     searchBtn.addEventListener('click', searchMessages);
+
     function searchMessages() {
         const searchQuery = searchInput.value.toLowerCase();
-        const filteredMessages = [];
-        snapshot.forEach(childSnapshot => {
-            if (childSnapshot.val().message.toLowerCase().includes(searchQuery.toLowerCase()))
-                filteredMessages.push(childSnapshot);
-        });
-
-        const searchResultsContainer = document.querySelector('#search-results-container');
-        searchResultsContainer.innerHTML = '';
-
-        filteredMessages.forEach(function (childSnapshot) {
-            const childData = childSnapshot.val();
-            const messageDiv = document.createElement('div');
-            messageDiv.innerText = childData.name + ": " + childData.message;
-            messageDiv.style.backgroundColor = childData.color;
-            messageDiv.classList.add("messageCard");
-            searchResultsContainer.appendChild(messageDiv);
-        });
-
-        const searchResultCount = document.querySelector('#search-result-count');
-        searchResultCount.innerText = `${filteredMessages.length} matching results`;
+        if (searchInput.value <= 0 ){
+            searchResultsContainer.innerHTML = '';
+            searchResultCount.innerText = ``;
+            searchErrorText.innerText = 'No inputs';
+        }
+        else{
+            const filteredMessages = [];
+            snapshot.forEach(childSnapshot => {
+                if (childSnapshot.val().message.toLowerCase().includes(searchQuery.toLowerCase()))
+                    filteredMessages.push(childSnapshot);
+            });
+    
+            searchResultsContainer.innerHTML = '';
+    
+            filteredMessages.forEach(function (childSnapshot) {
+                const childData = childSnapshot.val();
+                const messageDiv = document.createElement('div');
+                messageDiv.innerText = childData.name + ": " + childData.message;
+                messageDiv.style.backgroundColor = childData.color;
+                messageDiv.classList.add("messageCard");
+                searchResultsContainer.appendChild(messageDiv);
+            });
+    
+            searchResultCount.innerText = `${filteredMessages.length} matching results`;
+            searchInput.value = '';
+            searchErrorText.innerText = '';
+        }
     }
 });
